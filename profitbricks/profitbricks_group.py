83c60c64b48b03f49996e2d69807327e836b327b
nurfet-becirevic <nurfet@stackpointcloud.com>
2017-12-04 16:01:14 +0100

    Add group module

---
+++ profitbricks/profitbricks_group.py
@@ -0,0 +1,285 @@
+#!/usr/bin/python
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: profitbricks_group
+short_description: Create, update or remove a group.
+description:
+     - This module allows you to create, update or remove a group.
+version_added: "2.4"
+options:
+  name:
+    description:
+      - The name or ID of the group.
+    required: true
+  create_datacenter:
+    description:
+      - Boolean value indicating if the group is allowed to create virtual data centers.
+    required: false
+    default: None
+  create_snapshot:
+    description:
+      - Boolean value indicating if the group is allowed to create snapshots.
+    required: false
+    default: None
+  reserve_ip:
+    description:
+      - Boolean value indicating if the group is allowed to reserve IP addresses.
+    required: false
+    default: None
+  access_activity_log:
+    description:
+      - Boolean value indicating if the group is allowed to access the activity log.
+    required: false
+    default: None
+  subscription_user:
+    description:
+      - The ProfitBricks username. Overrides the PROFITBRICKS_USERNAME environment variable.
+    required: false
+  subscription_password:
+    description:
+      - The ProfitBricks password. Overrides the PROFITBRICKS_PASSWORD environment variable.
+    required: false
+  wait:
+    description:
+      - wait for the operation to complete before returning
+    required: false
+    default: "yes"
+    choices: [ "yes", "no" ]
+  wait_timeout:
+    description:
+      - how long before wait gives up, in seconds
+    default: 600
+  state:
+    description:
+      - Indicate desired state of the resource
+    required: false
+    default: "present"
+    choices: ["present", "absent"]
+
+requirements:
+    - "python >= 2.6"
+    - "profitbricks >= 4.0.0"
+'''
+
+EXAMPLES = '''
+# Create a group
+- name: Create group
+  profitbricks_group:
+    name: guests
+    create_datacenter: true
+    create_snapshot: true
+    reserve_ip: false
+    access_activity_log: false
+    state: present
+
+# Remove a group
+- name: Remove group
+  profitbricks_group:
+    name: guests
+    state: absent
+'''
+
+import os
+import time
+
+HAS_PB_SDK = True
+
+try:
+    from profitbricks import __version__ as sdk_version
+    from profitbricks.client import ProfitBricksService, Group
+except ImportError:
+    HAS_PB_SDK = False
+
+from ansible import __version__
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_native
+
+
+def _wait_for_completion(profitbricks, promise, wait_timeout, msg):
+    if not promise:
+        return
+    wait_timeout = time.time() + wait_timeout
+    while wait_timeout > time.time():
+        time.sleep(5)
+        operation_result = profitbricks.get_request(
+            request_id=promise['requestId'],
+            status=True)
+
+        if operation_result['metadata']['status'] == 'DONE':
+            return
+        elif operation_result['metadata']['status'] == 'FAILED':
+            raise Exception(
+                'Request failed to complete ' + msg + ' "' + str(
+                    promise['requestId']) + '" to complete.')
+
+    raise Exception('Timed out waiting for async operation ' + msg + ' "' +
+                    str(promise['requestId']) + '" to complete.')
+
+
+def create_update_group(module, profitbricks):
+    """
+    Creates or updates a group.
+
+    module : AnsibleModule object
+    profitbricks: authenticated profitbricks object.
+
+    Returns:
+        The group instance
+    """
+    name = module.params.get('name')
+    create_datacenter = module.params.get('create_datacenter')
+    create_snapshot = module.params.get('create_snapshot')
+    reserve_ip = module.params.get('reserve_ip')
+    access_activity_log = module.params.get('access_activity_log')
+    wait = module.params.get('wait')
+    wait_timeout = module.params.get('wait_timeout')
+
+    try:
+        group = None
+        for resource in profitbricks.list_groups()['items']:
+            if name in (resource['properties']['name'], resource['id']):
+                group = resource
+                break
+
+        if group:
+            if create_datacenter is None:
+                create_datacenter = group['properties']['createDataCenter']
+            if create_snapshot is None:
+                create_snapshot = group['properties']['createSnapshot']
+            if reserve_ip is None:
+                reserve_ip = group['properties']['reserveIp']
+            if access_activity_log is None:
+                access_activity_log = group['properties']['accessActivityLog']
+
+            group_response = profitbricks.update_group(
+                group_id=group['id'],
+                name=name,
+                create_datacenter=create_datacenter,
+                create_snapshot=create_snapshot,
+                reserve_ip=reserve_ip,
+                access_activity_log=access_activity_log
+            )
+        else:
+            group = Group(
+                name=name,
+                create_datacenter=create_datacenter or False,
+                create_snapshot=create_snapshot or False,
+                reserve_ip=reserve_ip or False,
+                access_activity_log=access_activity_log or False
+            )
+            group_response = profitbricks.create_group(group)
+
+        if wait:
+            _wait_for_completion(profitbricks, group_response,
+                                 wait_timeout, "create_update_group")
+
+        return {
+            'failed': False,
+            'changed': True,
+            'group': group_response
+        }
+
+    except Exception as e:
+        module.fail_json(msg="failed to create or update the group: %s" % to_native(e))
+
+
+def delete_group(module, profitbricks):
+    """
+    Removes a group
+
+    module : AnsibleModule object
+    profitbricks: authenticated profitbricks object.
+
+    Returns:
+        True if the group was removed, false otherwise
+    """
+    name = module.params.get('name')
+
+    # Locate UUID for the group
+    group_list = profitbricks.list_groups()
+    group_id = _get_resource_id(group_list, name)
+
+    try:
+        group_response = profitbricks.delete_group(group_id)
+        return group_response
+    except Exception as e:
+        module.fail_json(msg="failed to remove the group: %s" % to_native(e))
+
+
+def _get_resource_id(resource_list, identity):
+    """
+    Fetch and return the UUID of a resource regardless of whether the name or
+    UUID is passed.
+    """
+    for resource in resource_list['items']:
+        if identity in (resource['properties']['name'], resource['id']):
+            return resource['id']
+    return None
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            name=dict(type='str', required=True),
+            create_datacenter=dict(type='bool', default=None),
+            create_snapshot=dict(type='bool', default=None),
+            reserve_ip=dict(type='bool', default=None),
+            access_activity_log=dict(type='bool', default=None),
+            subscription_user=dict(type='str', default=os.environ.get('PROFITBRICKS_USERNAME')),
+            subscription_password=dict(type='str', default=os.environ.get('PROFITBRICKS_PASSWORD'), no_log=True),
+            wait=dict(type='bool', default=True),
+            wait_timeout=dict(type='int', default=600),
+            state=dict(type='str', default='present'),
+        )
+    )
+
+    if not HAS_PB_SDK:
+        module.fail_json(msg='profitbricks required for this module')
+
+    if not module.params.get('subscription_user'):
+        module.fail_json(msg='subscription_user parameter or ' +
+                             'PROFITBRICKS_USERNAME environment variable is required.')
+    if not module.params.get('subscription_password'):
+        module.fail_json(msg='subscription_password parameter or ' +
+                             'PROFITBRICKS_PASSWORD environment variable is required.')
+
+    subscription_user = module.params.get('subscription_user')
+    subscription_password = module.params.get('subscription_password')
+
+    profitbricks = ProfitBricksService(
+        username=subscription_user,
+        password=subscription_password)
+
+    user_agent = 'profitbricks-sdk-python/%s Ansible/%s' % (sdk_version, __version__)
+    profitbricks.headers = {'User-Agent': user_agent}
+
+    state = module.params.get('state')
+
+    if state == 'absent':
+        try:
+            (changed) = delete_group(module, profitbricks)
+            module.exit_json(changed=changed)
+        except Exception as e:
+            module.fail_json(msg='failed to set group state: %s' % to_native(e))
+
+    elif state == 'present':
+        try:
+            (group_dict) = create_update_group(module, profitbricks)
+            module.exit_json(**group_dict)
+        except Exception as e:
+            module.fail_json(msg='failed to set group state: %s' % to_native(e))
+
+
+if __name__ == '__main__':
+    main()
