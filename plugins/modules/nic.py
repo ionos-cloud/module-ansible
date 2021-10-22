ec539288f1251d45af0d3dec690ad7b0839bb159
mihaela-mh <48100626+mihaela-mh@users.noreply.github.com>
2021-10-22 08:57:42 +0300

    [v6] Remove dhcp default value (#14)

--- plugins/modules/nic.py
+++ plugins/modules/nic.py
@@ -171,7 +171,7 @@ def create_nic(module, client):
     datacenter = module.params.get('datacenter')
     server = module.params.get('server')
     lan = module.params.get('lan')
-    dhcp = module.params.get('dhcp') or False
+    dhcp = module.params.get('dhcp')
     firewall_active = module.params.get('firewall_active')
     ips = module.params.get('ips')
     name = module.params.get('name')
