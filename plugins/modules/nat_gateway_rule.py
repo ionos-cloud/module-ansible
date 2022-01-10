c9920272a7f5ecd838f8bf9b93df05042c308fa1
mihaela-mh <mihaela.mhv@gmail.com>
2022-01-10 14:01:26 +0200

    fix: fix deletion response for nat gateway rule

--- plugins/modules/nat_gateway_rule.py
+++ plugins/modules/nat_gateway_rule.py
@@ -27,6 +27,20 @@ uuid_match = re.compile(
     '[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}', re.I)
 
 
+def _get_resource(resource_list, identity):
+    """
+    Fetch and return a resource regardless of whether the name or
+    UUID is passed. Returns None error otherwise.
+    """
+
+    for resource in resource_list.items:
+        if identity in (resource.properties.name, resource.id):
+            return resource.id
+
+    return None
+
+
+
 def _update_nat_gateway_rule(module, client, nat_gateway_server, datacenter_id, nat_gateway_id, nat_gateway_rule_id,
                              nat_gateway_rule_properties):
     wait = module.params.get('wait')
@@ -203,35 +217,25 @@ def remove_nat_gateway_rule(module, client):
     changed = False
 
     try:
+        nat_gateway_rule_list = nat_gateway_server.datacenters_natgateways_rules_get(datacenter_id=datacenter_id, nat_gateway_id=nat_gateway_id, depth=5)
         if nat_gateway_rule_id:
-            response = nat_gateway_server.datacenters_natgateways_rules_delete_with_http_info(datacenter_id,
-                                                                                              nat_gateway_id,
-                                                                                              nat_gateway_rule_id)
-            (nat_gateway_rule_response, _, headers) = response
+            nat_gateway_rule = _get_resource(nat_gateway_rule_list, nat_gateway_rule_id)
+        else:
+            nat_gateway_rule = _get_resource(nat_gateway_rule_list, name)
 
-            if wait:
-                request_id = _get_request_id(headers['Location'])
-                client.wait_for_completion(request_id=request_id, timeout=wait_timeout)
+        if not nat_gateway_rule:
+            module.exit_json(changed=False)
 
-            changed = True
+        response = nat_gateway_server.datacenters_natgateways_rules_delete_with_http_info(datacenter_id,
+                                                                                          nat_gateway_id,
+                                                                                          nat_gateway_rule)
+        (nat_gateway_rule_response, _, headers) = response
 
-        elif name:
-            nat_gateway_rules = nat_gateway_server.datacenters_natgateways_rules_get(datacenter_id=datacenter_id,
-                                                                                     nat_gateway_id=nat_gateway_id,
-                                                                                     depth=2)
-            for rule in nat_gateway_rules.items:
-                if name == rule.properties.name:
-                    nat_gateway_rule_id = rule.id
-                    response = nat_gateway_server.datacenters_natgateways_rules_delete_with_http_info(datacenter_id,
-                                                                                                      nat_gateway_id,
-                                                                                                      nat_gateway_rule_id)
-                    (nat_gateway_rule_response, _, headers) = response
+        if wait:
+            request_id = _get_request_id(headers['Location'])
+            client.wait_for_completion(request_id=request_id, timeout=wait_timeout)
 
-                    if wait:
-                        request_id = _get_request_id(headers['Location'])
-                        client.wait_for_completion(request_id=request_id, timeout=wait_timeout)
-
-                    changed = True
+        changed = True
 
     except Exception as e:
         module.fail_json(
