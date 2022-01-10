73fda622a2416538457d4bdbee58f4aa6b055179
mihaela-mh <mihaela.mhv@gmail.com>
2022-01-10 14:41:57 +0200

    fix: fix deletion response for nlb forwarding rule

--- plugins/modules/network_load_balancer_rule.py
+++ plugins/modules/network_load_balancer_rule.py
@@ -28,6 +28,19 @@ uuid_match = re.compile(
     '[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}', re.I)
 
 
+def _get_resource(resource_list, identity):
+    """
+    Fetch and return a resource regardless of whether the name or
+    UUID is passed. Returns None error otherwise.
+    """
+
+    for resource in resource_list.items:
+        if identity in (resource.properties.name, resource.id):
+            return resource.id
+
+    return None
+
+
 def _update_nlb_forwarding_rule(module, client, nlb_server, datacenter_id, network_load_balancer_id, forwarding_rule_id,
                                 forwarding_rule_properties):
     wait = module.params.get('wait')
@@ -215,37 +228,26 @@ def remove_nlb_forwarding_rule(module, client):
     changed = False
 
     try:
+        network_load_balancer_rule_list = nlb_server.datacenters_networkloadbalancers_forwardingrules_get(datacenter_id=datacenter_id, network_load_balancer_id=network_load_balancer_id, depth=5)
         if forwarding_rule_id:
-            response = nlb_server.datacenters_networkloadbalancers_forwardingrules_delete_with_http_info(datacenter_id,
-                                                                                                         network_load_balancer_id,
-                                                                                                         forwarding_rule_id)
-            (forwarding_rule_response, _, headers) = response
+            network_load_balancer_rule = _get_resource(network_load_balancer_rule_list, forwarding_rule_id)
+        else:
+            network_load_balancer_rule = _get_resource(network_load_balancer_rule_list, name)
 
-            if wait:
-                request_id = _get_request_id(headers['Location'])
-                client.wait_for_completion(request_id=request_id, timeout=wait_timeout)
+        if not network_load_balancer_rule:
+            module.exit_json(changed=False)
 
-            changed = True
+        response = nlb_server.datacenters_networkloadbalancers_forwardingrules_delete_with_http_info(datacenter_id,
+                                                                                                     network_load_balancer_id,
+                                                                                                     network_load_balancer_rule)
+        (forwarding_rule_response, _, headers) = response
 
-        elif name:
-            forwarding_rules = nlb_server.datacenters_networkloadbalancers_forwardingrules_get(
-                                                            datacenter_id=datacenter_id,
-                                                            network_load_balancer_id=network_load_balancer_id,
-                                                            depth=2)
-            for rule in forwarding_rules.items:
-                if name == rule.properties.name:
-                    forwarding_rule_id = rule.id
-                    response = nlb_server.datacenters_networkloadbalancers_forwardingrules_delete_with_http_info(
-                                                            datacenter_id,
-                                                            network_load_balancer_id,
-                                                            forwarding_rule_id)
-                    (forwarding_rule_response, _, headers) = response
+        if wait:
+            request_id = _get_request_id(headers['Location'])
+            client.wait_for_completion(request_id=request_id, timeout=wait_timeout)
 
-                    if wait:
-                        request_id = _get_request_id(headers['Location'])
-                        client.wait_for_completion(request_id=request_id, timeout=wait_timeout)
+        changed = True
 
-                    changed = True
 
     except Exception as e:
         module.fail_json(
