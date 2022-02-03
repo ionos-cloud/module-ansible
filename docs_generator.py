98f8b0b1a68fe8b00791d0ff0c2d4a33f99459cc
Radu Mocanu <radu_mocanu2000@yahoo.com>
2022-02-03 17:56:38 +0200

    doc: refactor doc generator

--- docs_generator.py
+++ docs_generator.py
@@ -1,5 +1,4 @@
-from plugins.modules.datacenter import OPTIONS, DOCUMENTATION, EXAMPLES, STATES, DOC_DIRECTORY, EXAMPLE_PER_STATE
-
+import importlib
 import yaml
 import chevron
 import copy
@@ -7,33 +6,48 @@ import os
 from pathlib import Path
 
 
-parameters = []
+def generate_module_docs(module_name):
+  for module_name in modules_to_generate:
+    module = importlib.import_module('plugins.modules.' + module_name)
 
-for state in STATES:
-    def available_in_state(option):
-        return state in option[1]['available']
-    state_parameters = []
-    for el in list(filter(available_in_state, copy.deepcopy(OPTIONS).items())):
-        el[1]['name'] = el[0]
-        el[1]['description'] = ''.join(el[1]['description'])
-        el[1]['required'] = state in el[1].get('required', [])
-        state_parameters.append(el[1])
-    parameters.append({
-        'state': state,
-        'parameters': state_parameters,
-        'example': EXAMPLE_PER_STATE.get(state),
-    })
+    parameters = []
 
-with open(os.path.join('docs', os.path.join('templates', 'module.mustache')), 'r') as template_file:
-    target_filename = os.path.join('docs', os.path.join('api', DOC_DIRECTORY))
-    Path(target_filename).mkdir(parents=True, exist_ok=True)
+    for state in module.STATES:
+        def available_in_state(option):
+            return state in option[1]['available']
+        state_parameters = []
+        for el in list(filter(available_in_state, copy.deepcopy(module.OPTIONS).items())):
+            el[1]['name'] = el[0]
+            el[1]['description'] = ''.join(el[1]['description'])
+            el[1]['required'] = state in el[1].get('required', [])
+            state_parameters.append(el[1])
+        parameters.append({
+            'state': state,
+            'parameters': state_parameters,
+            'example': module.EXAMPLE_PER_STATE.get(state),
+        })
 
-    with open(os.path.join(target_filename, 'datacenter.md'), 'w') as target_file:
-        target_file.write(chevron.render(
-            template_file,
-            {
-                'description': ''.join(yaml.safe_load(DOCUMENTATION)['description']),
-                'example': EXAMPLES,
-                'states': parameters,
-            },
-        ))
+    with open(os.path.join('docs', os.path.join('templates', 'module.mustache')), 'r') as template_file:
+        target_directory = os.path.join('docs', os.path.join('api', module.DOC_DIRECTORY))
+        Path(target_directory).mkdir(parents=True, exist_ok=True)
+        target_filename = os.path.join(target_directory, module_name + '.md')
+
+        with open(target_filename, 'w') as target_file:
+            target_file.write(chevron.render(
+                template_file,
+                {
+                    'description': ''.join(yaml.safe_load(module.DOCUMENTATION)['description']),
+                    'example': module.EXAMPLES,
+                    'states': parameters,
+                },
+            ))
+            print('Generated docs for <{}> in {}'.format(module_name, target_filename))
+    return module.DOC_DIRECTORY, target_filename
+
+modules_to_generate = [
+  'datacenter'
+]
+
+for module_name in modules_to_generate:
+  generate_module_docs(module_name)
+
