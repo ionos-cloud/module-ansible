7553d30766b5582fe9eb9e6c6462077331740583
Radu Mocanu <radu_mocanu2000@yahoo.com>
2022-05-09 11:52:37 +0300

    feat: checking multiple matches for lan

--- plugins/modules/lan.py
+++ plugins/modules/lan.py
@@ -172,6 +172,45 @@ EXAMPLE_PER_STATE = {
 EXAMPLES = '\n'.join(EXAMPLE_PER_STATE.values())
 
 
+def _get_matched_resources(resource_list, identity, identity_paths=None):
+    """
+    Fetch and return a resource based on an identity supplied for it, if none or more than one matches 
+    are found an error is printed and None is returned.
+    """
+
+    if identity_paths is None:
+      identity_paths = [['id'], ['properties', 'name']]
+
+    def check_identity_method(resource):
+      resource_identity = []
+
+      for identity_path in identity_paths:
+        current = resource
+        for el in identity_path:
+          current = getattr(current, el)
+        resource_identity.append(current)
+
+      return identity in resource_identity
+
+    return list(filter(check_identity_method, resource_list.items))
+
+
+def get_resource(module, resource_list, identity, identity_paths=None):
+    matched_resources = _get_matched_resources(resource_list, identity, identity_paths)
+
+    if len(matched_resources) == 1:
+        return matched_resources[0]
+    elif len(matched_resources) > 1:
+        module.fail_json("found more resources of type {} for '{}'".format(resource_list.id, identity))
+    else:
+        return None
+
+
+def get_resource_id(*args, **kwargs):
+    resource = get_resource(*args, **kwargs)
+    return resource.id if resource is not None else None
+
+
 def _get_request_id(headers):
     match = re.search('/requests/([-A-Fa-f0-9]+)/', headers)
     if match:
@@ -202,36 +241,28 @@ def create_lan(module, client):
 
     # Locate UUID for virtual datacenter
     datacenter_list = datacenter_server.datacenters_get(depth=2)
-    datacenter_id = _get_resource_id(datacenter_list, datacenter, module, "Data center")
+    datacenter_id = get_resource_id(module, datacenter_list, datacenter)
 
     lan_list = lan_server.datacenters_lans_get(datacenter_id, depth=2)
-    lan = None
-    for i in lan_list.items:
-        if name == i.properties.name:
-            lan = i
-            break
 
-    should_change = lan is None
+    existing_lan = get_resource(module, lan_list, name)
 
-    if module.check_mode:
-        module.exit_json(changed=should_change)
-
-    if not should_change:
+    if existing_lan:
         return {
-            'changed': should_change,
+            'changed': False,
             'failed': False,
             'action': 'create',
-            'lan': lan.to_dict(),
+            'lan': existing_lan.to_dict(),
         }
+
+    if module.check_mode:
+        module.exit_json(changed=False)
+
     lan_response = None
     try:
-        lan_properties = LanPropertiesPost(name=name,
-                                           public=public)
+        lan = LanPost(properties=LanPropertiesPost(name=name, public=public))
 
-        lan = LanPost(properties=lan_properties)
-
-        response = lan_server.datacenters_lans_post_with_http_info(datacenter_id=datacenter_id, lan=lan)
-        (lan_response, _, headers) = response
+        lan_response, _, headers = lan_server.datacenters_lans_post_with_http_info(datacenter_id=datacenter_id, lan=lan)
 
         request_id = _get_request_id(headers['Location'])
         client.wait_for_completion(request_id=request_id, timeout=wait_timeout)
@@ -270,11 +301,11 @@ def update_lan(module, client):
 
     # Locate UUID for virtual datacenter
     datacenter_list = datacenter_server.datacenters_get(depth=2)
-    datacenter_id = _get_resource_id(datacenter_list, datacenter, module, "Data center")
+    datacenter_id = get_resource_id(module, datacenter_list, datacenter)
 
     # Prefetch a list of LANs.
     lan_list = lan_server.datacenters_lans_get(datacenter_id, depth=2)
-    lan_id = _get_resource_id(lan_list, name, module, "LAN")
+    lan_id = get_resource_id(module, lan_list, name, module)
 
     if module.check_mode:
         module.exit_json(changed=True)
@@ -323,16 +354,14 @@ def delete_lan(module, client):
 
     # Locate UUID for virtual datacenter
     datacenter_list = datacenter_server.datacenters_get(depth=2)
-    datacenter_id = _get_resource_id(datacenter_list, datacenter, module, "Data center")
+    datacenter_id = get_resource_id(module, datacenter_list, datacenter)
 
     # Locate ID for LAN
     lan_list = lan_server.datacenters_lans_get(datacenter_id=datacenter_id, depth=5)
-    lan = _get_resource(lan_list, name)
+    lan_id = get_resource_id(module, lan_list, name)
 
-    if not lan:
+    if not lan_id:
         module.exit_json(changed=False)
-
-    lan_id = _get_resource_id(lan_list, name, module, "LAN")
 
     if module.check_mode:
         module.exit_json(changed=True)
@@ -346,31 +375,6 @@ def delete_lan(module, client):
         }
     except Exception as e:
         module.fail_json(msg="failed to remove the LAN: %s" % to_native(e))
-
-
-def _get_resource_id(resource_list, identity, module, resource_type):
-    """
-    Fetch and return the UUID of a resource regardless of whether the name or
-    UUID is passed. Throw an error otherwise.
-    """
-    for resource in resource_list.items:
-        if identity in (resource.properties.name, resource.id):
-            return resource.id
-
-    module.fail_json(msg='%s \'%s\' could not be found.' % (resource_type, identity))
-
-
-def _get_resource(resource_list, identity):
-    """
-    Fetch and return a resource regardless of whether the name or
-    UUID is passed. Returns None error otherwise.
-    """
-
-    for resource in resource_list.items:
-        if identity in (resource.properties.name, resource.id):
-            return resource.id
-
-    return None
 
 
 def get_module_arguments():
